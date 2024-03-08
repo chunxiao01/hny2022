ded016008a5e4dba2d6e09d8766db24bedfa19af
chunxiao01 <12861187+chunxiao01@users.noreply.github.com>
2024-03-08 23:56:42 +0800

    the first commit.

---
+++ src/Views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="home">
+    <div class="img-container">
+      <img ref="imgref" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue"
+import imgUrl from "@/assets/img/2022.jpg"
+
+const imgref = ref(null)
+
+onMounted(() => {
+  imgref.value.src = imgUrl
+})
+</script>
+
+<style scoped>
+.home {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  flex-direction: column;
+  z-index: 10;
+}
+.img-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  overflow: hidden;
+}
+.img-container img {
+  width: 100%;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,48 @@
+import { fileURLToPath, URL } from "node:url"
+
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import { VitePWA } from "vite-plugin-pwa"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "新年快乐2022",
+        description: "新年快乐，虎年大吉🐅！祝你天天开心，天天可爱！",
+        theme_color: "#222222",
+        icons: [
+          {
+            src: "/hny2022/icon192.svg",
+            sizes: "192x192",
+            type: "image/svg+xml"
+          },
+          {
+            src: "/hny2022/icon512.svg",
+            sizes: "512x512",
+            type: "image/svg+xml"
+          }
+        ]
+      },
+
+      devOptions: {
+        // 如果想在`vite dev`命令下调试PWA, 必须启用它
+        enabled: true,
+        // Vite在dev模式下会使用浏览器原生的ESModule，将type设置为`"module"`与原先的保持一致
+        type: "module"
+      }
+    })
+  ],
+  base: "/hny2022/",
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
+  },
+  server: {
+    host: "0.0.0.0"
+  }
+})
